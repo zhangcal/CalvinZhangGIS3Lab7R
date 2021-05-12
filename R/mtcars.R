4c09f4f63763cb95cc86bc0c5b647b06d7768b22
zhangcal <zhangcal@uchicago.edu>
2021-05-12 17:04:26 +0800

    Added in tutorial files

---
+++ R/mtcars.R
@@ -0,0 +1,10 @@
+#' A list of 1000 randomly generated numbes
+#'
+#'
+#' @format Mtcars dataset from R
+#' \describe{
+#'   \item{mpg}{numeric, 	Miles/(US) gallon}
+#'   \item{cyl}{numeric, Number of cylinders}
+#'   ...
+#' }
+"x"
---
+++ R/x.R
@@ -0,0 +1,10 @@
+#' A list of 1000 randomly generated numbes
+#'
+#'
+#' @format mtcars: Motor Trend Car Road Tests
+#' \describe{
+#'   \item{x}{integer}
+#'   ...
+#' }
+#' @source \url{https://www.rdocumentation.org/packages/datasets/versions/3.6.2/topics/mtcars}
+"mtcars"
